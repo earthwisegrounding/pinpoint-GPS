84f4edef7ea1e9ae513853e1ac8d66307f670521
earthwisegrounding <emfearthwisegrounding@gmail.com>
2026-09-20 05:03:57 -0700

    Update config.js

--- dist/config.js
+++ dist/config.js
@@ -1,5 +1,5 @@
 // Public connection settings only. Never put a password or service-role key here.
 window.PINPOINT_CONFIG = {
-  supabaseUrl: '',
-  supabaseKey: ''
+  supabaseUrl: 'https://uwxpwlqggpqeaulqkpal.supabase.co',
+  supabaseKey: 'sb_publishable_9l5CQWP3a0k9hkFkYf8-Rg_VRCHY5bh'
 };
